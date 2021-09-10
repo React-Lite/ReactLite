690bbb7afe8070d466d004919bb337999d2b6358
Guga-innecco <87543901+Guga-innecco@users.noreply.github.com>
2021-09-10 10:50:23 -0300

    Update react.js

--- react.js
+++ react.js
@@ -41,37 +41,3 @@ function createElement(elementType, props, ...children) {
 const React = {
   createElement,
 };
-
-// ========================================
-
-function Title() {
-  return React.createElement("h1", null, "Bla");
-}
-
-function App(props) {
-  // return (
-  //   React.createElement(
-  //     "section",
-  //     {
-  //       className: "App"
-  //     }, 
-  //     React.createElement(Title, null), 
-  //     React.createElement("div", null, 
-  //     React.createElement("div", null, "0"), 
-  //     React.createElement("button", null, "Incrementar"), 
-  //     React.createElement("button", null, "Decrementar"))
-  //   )
-  // );
-  return (
-    <section className="App">
-      <h1>Contador</h1>
-      <div>
-        <div>0</div>
-        <button>Incrementar</button>
-        <button>Decrementar</button>
-      </div>
-    </section>
-  );
-}
-
-render(React.createElement(App, null), document.querySelector('#root'));
